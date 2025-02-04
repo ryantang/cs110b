80f146f843c0f9f6b4f54e8f9dde1bc2c6bd0e62
Ryan Tang <ryantang@gmail.com>
2025-02-03 22:04:58 -0800

    got three of a kind; extracted countCards

--- project13.1/main.cpp
+++ project13.1/main.cpp
@@ -10,6 +10,7 @@ const int HIGHEST_NUM = 9;
 bool containsPair(const int hand[]);
 bool containsTwoPair(const int hand[]);
 bool containsThreeOfaKind(const int hand[]);
+void countCards(std::__1::vector<int> &cardsByValues, const int hand[]);
 bool containsStraight(const int hand[]);
 bool containsFullHouse(const int hand[]);
 bool containsFourOfaKind(const int hand[]);
@@ -20,24 +21,50 @@ int main() {
 }
 
 bool containsPair(const int hand[]) {
-    // Create a vector to store the number of cards with each value, 
-    // using the value as the index. 
-    // For example, if hand is {2, 3, 3, 5, 5},
-    // cardsWithValues[2] = 1 (there is one two), 
-    // cardsWithValues[3] = 2 (there are two three's),
-    // cardsWithValues[5] = 2 (there are two fives).
-    vector<int> cardsWithValues(HIGHEST_NUM + 1);
-    for (int i = 0; i < HAND_SIZE; i++) {
-        cardsWithValues[hand[i]]++;
-    }
+    vector<int> cardsByValues(HIGHEST_NUM + 1);
+    countCards(cardsByValues, hand);
     for (int i = LOWEST_NUM; i <= HIGHEST_NUM; i++) {
-        if (cardsWithValues[i] == 2) {
+        if (cardsByValues[i] == 2) {
             return true;
         }
     }
     return false;
 }
 
+bool containsTwoPair(const int hand[]) {
+    vector<int> cardsByValues(HIGHEST_NUM + 1);
+    int pairs = 0;
+    countCards(cardsByValues, hand);
+    for (int i = LOWEST_NUM; i <= HIGHEST_NUM; i++) {
+        if (cardsByValues[i] == 2) {
+            pairs++;
+        }
+    }
+    return pairs == 2;
+}
+
+bool containsThreeOfaKind(const int hand[]) {
+    vector<int> cardsByValues(HIGHEST_NUM + 1);
+    countCards(cardsByValues, hand);
+    for (int i = LOWEST_NUM; i <= HIGHEST_NUM; i++) {
+        if (cardsByValues[i] == 3) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void countCards(vector<int> &cardsByValues, const int hand[]) {
+    // Create a vector to store the number of cards with each value, 
+    // using the value as the index. 
+    // For example, if hand is {2, 3, 3, 5, 5},
+    // cardsByValues[2] = 1 (there is one two), 
+    // cardsByValues[3] = 2 (there are two three's),
+    // cardsByValues[5] = 2 (there are two fives).
+    for (int i = 0; i < HAND_SIZE; i++) {
+        cardsByValues[hand[i]]++;
+    }
+}
 
 void runTests() {
     int highCard[5] = {6, 2, 3, 4, 9};
@@ -45,6 +72,9 @@ void runTests() {
     int twoPair[5] = {7, 5, 3, 7, 5};
     int threeOfAKind[5] = {3, 5, 3, 3, 8};
     int straight[5] = {2, 3, 4, 5, 6};
+    int fullHouse[5] = {3, 3, 3, 5, 5};
+    int fourOfAKind[5] = {3, 3, 3, 3, 5};
+    int fiveOfAKind[5] = {3, 3, 3, 3, 3};
     // int outOfOrderStraight[5] = {8, 5, 4, 6, 7};
 
     assert(!containsPair(highCard));
@@ -52,6 +82,19 @@ void runTests() {
     assert(containsPair(twoPair));
     assert(!containsPair(threeOfAKind));
     assert(!containsPair(straight));
+
+    assert(!containsTwoPair(highCard));
+    assert(!containsTwoPair(pair));
+    assert(containsTwoPair(twoPair));
+    assert(!containsTwoPair(threeOfAKind));
+
+    assert(!containsThreeOfaKind(highCard));
+    assert(!containsThreeOfaKind(pair));
+    assert(!containsThreeOfaKind(twoPair));
+    assert(containsThreeOfaKind(threeOfAKind));
+    assert(containsThreeOfaKind(fullHouse));
+    assert(!containsThreeOfaKind(fourOfAKind));
+    assert(!containsThreeOfaKind(fiveOfAKind));
 }
 
 
