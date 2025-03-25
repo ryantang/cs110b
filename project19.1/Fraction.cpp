0ef94abe28685dab56636f6bfc5c8be093b47cd1
Ryan Tang <ryantang@gmail.com>
2025-03-24 17:40:17 -0700

    add comments and small refactors

--- project19.1/Fraction.cpp
+++ project19.1/Fraction.cpp
@@ -1,3 +1,10 @@
+/*
+The Fraction class has two private data members, numerator and denominator, that store the values of the fraction. The class stores
+the fraction in its simplest form, meaning the numerator and denominator have no common factors other than 1. However, it still stores
+the fraction as an "improper fraction", meaning the numerator can be greater than the denominator. The insertion operator (<<) will 
+convert the fraction to a mixed number if necessary. This class handles negative fractions.
+*/
+
 #include <iostream>
 #include <cassert>
 #include <cstdlib>
@@ -13,47 +20,102 @@ namespace cs_fraction {
         simplify();
     }
 
+
+
+
+
+    
     bool operator==(const Fraction& left, const Fraction& right) {
         return left.numerator * right.denominator == right.numerator * left.denominator;
     }
+
+
+
+
+
 
     bool operator<(const Fraction& left, const Fraction& right) {
         return left.numerator * right.denominator < right.numerator * left.denominator;
     }
 
+
+
+
+
+
     bool operator<=(const Fraction& left, const Fraction& right) {
         return left.numerator * right.denominator <= right.numerator * left.denominator;
     }
+
+
+
+
+
 
     bool operator>(const Fraction& left, const Fraction& right) {
         return left.numerator * right.denominator > right.numerator * left.denominator;
     }
 
+
+
+
+
+
     bool operator>=(const Fraction& left, const Fraction& right) {
         return left.numerator * right.denominator >= right.numerator * left.denominator;
     }
 
+
+
+
+
+
     bool operator!=(const Fraction& left, const Fraction& right) {
         return left.numerator * right.denominator != right.numerator * left.denominator;
     }
+
+
+
+
+
 
     Fraction operator+(const Fraction& left, const Fraction& right) {
         return Fraction(left.numerator * right.denominator + right.numerator * left.denominator, 
             left.denominator * right.denominator);
     }
 
+
+
+
+
+
     Fraction operator-(const Fraction& left, const Fraction& right) {
         return Fraction(left.numerator * right.denominator - right.numerator * left.denominator, 
             left.denominator * right.denominator);
     }
 
+
+
+
+
+
     Fraction operator*(const Fraction& left, const Fraction& right) {
         return Fraction(left.numerator * right.numerator, left.denominator * right.denominator);
     }
 
+
+
+
+
+
     Fraction operator/(const Fraction& left, const Fraction& right) {
         return Fraction(left.numerator * right.denominator, left.denominator * right.numerator);
     }
+
+
+
+
+
 
     Fraction& Fraction::operator+=(const Fraction& f) {
         numerator = numerator * f.denominator + f.numerator * denominator;
@@ -62,12 +124,22 @@ namespace cs_fraction {
         return *this;
     }
 
+
+
+
+
+
     Fraction& Fraction::operator-=(const Fraction& f) {
         numerator = numerator * f.denominator - f.numerator * denominator;
         denominator = denominator * f.denominator;
         simplify();
         return *this;
     }
+
+
+
+
+    
 
     Fraction& Fraction::operator*=(const Fraction& f) {
         numerator = numerator * f.numerator;
@@ -76,6 +148,11 @@ namespace cs_fraction {
         return *this;
     }
 
+
+
+
+
+
     Fraction& Fraction::operator/=(const Fraction& f) {
         numerator = numerator * f.denominator;
         denominator = denominator * f.numerator;
@@ -83,11 +160,21 @@ namespace cs_fraction {
         return *this;
     }
 
+
+
+
+
+
     Fraction& Fraction::operator++() {
         numerator += denominator;
         simplify();
         return *this;
     }
+
+
+
+
+
 
     Fraction Fraction::operator++(int) {
         Fraction temp = *this;
@@ -96,11 +183,21 @@ namespace cs_fraction {
         return temp;
     }
 
+
+
+
+
+
     Fraction& Fraction::operator--() {
         numerator -= denominator;
         simplify();
         return *this;
     }
+
+
+
+
+
 
     Fraction Fraction::operator--(int) {
         Fraction temp = *this;
@@ -109,12 +206,24 @@ namespace cs_fraction {
         return temp;
     }
 
+
+
+
+
+
     void Fraction::testSimplify(int expectedNumerator, int expectedDenominator) {
         simplify();
         assert(numerator == expectedNumerator);
         assert(denominator == expectedDenominator);
     }
 
+
+
+
+
+    // This extraction operator calculates whether the input has a non-zero whole number or non-zero remainder.
+    // If it has both, it prints out a mixed number (1+3/4). If it has only a whole number, it prints out the whole number (5).
+    // If it has only a remainder, it prints out a proper fraction (1/2).
     std::ostream& operator<<(ostream& out, const Fraction& f) {
         int whole = f.numerator / f.denominator;
         int remainder = f.numerator % f.denominator;
@@ -123,26 +232,26 @@ namespace cs_fraction {
         } else if (whole == 0) {
             out << remainder << "/" << f.denominator;
         } else { 
-                out << whole << "+" << abs(remainder) << "/" << f.denominator;
+            out << whole << "+" << abs(remainder) << "/" << f.denominator;
         }
         return out;
     }
 
+
+
+
+
+
+    // This insertion operator reads in a stream, converts them to individual strings, and parses them into
+    // Fraction objects. If it detects a "+" sign, it will parse the string as a mixed number. Otherwise, it 
+    // detects a "/" sign, it will parse the string as a fraction. If it doesn't detect either, it will parse 
+    // the string as a whole number.
     std::istream& operator>>(istream& in, Fraction& f) {
         string input;
-        int whole, numerator, denominator;
+        int numerator, denominator;
         while(in >> input) {
             if (input.find("+") != string::npos) {
-                int plusIndex = input.find("+");
-                int slashIndex = input.find("/");
-                whole = stoi(input.substr(0, plusIndex));
-                numerator = stoi(input.substr(plusIndex + 1, slashIndex));
-                denominator = stoi(input.substr(slashIndex + 1));
-                if (whole < 0) {
-                    f = Fraction(whole * denominator - numerator, denominator);
-                } else {
-                    f = Fraction(whole * denominator + numerator, denominator);
-                }   
+                f.parseMixedNumber(input);
                 return in;
             } else if (input.find("/") != string::npos) {
                 int slashIndex = input.find("/");
@@ -159,6 +268,44 @@ namespace cs_fraction {
     }
 
 
+
+
+
+
+    // The parsedMixedNumber function takes a mixed number string (1+1/2) and parses it into a Fraction object.
+    // It first finds the "+" and "/" signs, then parses the whole number, numerator, and denominator. It then
+    // calculates the numerator adjusting for negatives.
+    void Fraction::parseMixedNumber(string input) {
+        int plusIndex = input.find("+");
+        int slashIndex = input.find("/");
+        int whole = stoi(input.substr(0, plusIndex));
+        int parsedNumerator = stoi(input.substr(plusIndex + 1, slashIndex));
+
+        denominator = stoi(input.substr(slashIndex + 1));
+        if (whole < 0) {
+            numerator = whole * denominator - parsedNumerator;
+        } else {
+            numerator = whole * denominator + parsedNumerator;
+        }
+        simplify();   
+    }
+    
+
+
+
+
+
+    // The simplify function simplifies the fraction to its lowest terms. It does not return anything.
+    //
+    // Implementation: Since we allow negative and improper fractions, we start with the 
+    // absolute value of the numerator and denominator, and find the smaller of the two. 
+    // We then loop from that number down to 2, looking for the greatest common factor (GCF). 
+    // When we find it, we divide both the numerator and denominator by the GCF.
+    //
+    // The normalize function ensures that the denominator is positive.
+    //
+    // This function also handles the case where the numerator is 0. In that case, the denominator
+    // is set to 1.
     void Fraction::simplify() {
         int smaller;
         bool foundGCF = false;
@@ -168,10 +315,10 @@ namespace cs_fraction {
             denominator = 1;
         }
 
-        if (abs(numerator) < abs(denominator)) {
+        if (abs(numerator) < denominator) {
             smaller = abs(numerator);
         } else {
-            smaller = abs(denominator);
+            smaller = denominator;
         }
 
         for (int i = smaller; i > 1; i--) {
@@ -183,6 +330,12 @@ namespace cs_fraction {
         }
     }
 
+
+
+
+
+
+    // The normalize function ensures that the denominator is positive.
     void Fraction::normalize() {
         if (denominator < 0) {
             numerator = -numerator;
--- project19.1/Fraction.h
+++ project19.1/Fraction.h
@@ -1,3 +1,156 @@
+/* Ryan Tang | 3/24/2025 | CS 110 with instructor David Harden | project 19.1/Fraction.h
+
+This header file declares the Fraction class. The Fraction class has a single contructor that takes
+two integers as parameters (numerator and denominator), and initializes the Fraction object to the given values.
+This class can handle negative fractions. It will simplify the fraction to its lowest terms and normalize 
+negative denominators.
+
+This Fraction class overloads six relational operators (==, <, <=, >, >=, !=), four basic arithmetic 
+operators (+, -, *, /), shorthand arithmetic operators (+=, -=, *=, /=), the pre-and-post increment 
+and decrement operators (++, --), and the insertion and deletion operators (<<, >>).
+
+
+void Fraction::Fraction(int setNumerator, int setDenominator)
+
+Precondition: The denominator cannot be 0. Requires up to two integers as parameters.
+Postcondition: Initializes the Fraction object to the given values. Simplifies the fraction to its lowest terms. Zero parameters
+    will initialize the Fraction object to 0/1. One parameter will treat the parameter as a whole number and set the denominator to 1.
+    Two parameters will set the numerator and denominator to the given values.
+
+
+bool operator==(const Fraction& left, const Fraction& right)
+
+Precondition: Requires two valid Fraction objects.
+Postcondition: Returns a boolean value indicating if the two fractions are equal.
+
+
+bool operator<(const Fraction& left, const Fraction& right)
+
+Precondition: Requires two valid Fraction objects.
+Postcondition: Returns a boolean value indicating if the left fraction is less than the right fraction.
+
+
+bool operator<=(const Fraction& left, const Fraction& right)
+
+Precondition: Requires two valid Fraction objects.
+Postcondition: Returns a boolean value indicating if the left fraction is less than or equal to the right fraction.
+
+
+bool operator>(const Fraction& left, const Fraction& right)
+
+Precondition: Requires two valid Fraction objects.
+Postcondition: Returns a boolean value indicating if the left fraction is greater than the right fraction.
+
+
+bool operator>=(const Fraction& left, const Fraction& right)
+
+Precondition: Requires two valid Fraction objects.
+Postcondition: Returns a boolean value indicating if the left fraction is greater than or equal to the right fraction.
+
+
+bool operator!=(const Fraction& left, const Fraction& right)
+
+Precondition: Requires two valid Fraction objects.
+Postcondition: Returns a boolean value indicating if the two fractions are not equal
+
+
+Fraction operator+(const Fraction& left, const Fraction& right)
+
+Precondition: Requires two valid Fraction objects, or one Fraction object and one integer.
+Postcondition: Returns a Fraction object that is the sum of the two given parameters.
+
+
+Fraction operator-(const Fraction& left, const Fraction& right)
+
+Precondition: Requires two valid Fraction objects, or one Fraction object and one integer.
+Postcondition: Returns a Fraction object that is the difference of the two given parameters.
+
+
+Fraction operator*(const Fraction& left, const Fraction& right)
+
+Precondition: Requires two valid Fraction objects, or one Fraction object and one integer.
+Postcondition: Returns a Fraction object that is the product of the two given parameters.
+
+
+Fraction operator/(const Fraction& left, const Fraction& right)
+
+Precondition: Requires two valid Fraction objects, or one Fraction object and one integer.
+Postcondition: Returns a Fraction object that is the quotient of the two given parameters.
+
+
+Fraction& operator+=(const Fraction& f)
+
+Precondition: Requires a valid Fraction object and parameter that's a valid Fraction object or integer.
+Postcondition: Adds the given parameter to the Fraction object and returns the result.
+
+
+Fraction& operator-=(const Fraction& f)
+
+Precondition: Requires a valid Fraction object and parameter that's a valid Fraction object or integer.
+Postcondition: Subtracts the given parameter from the Fraction object and returns the result.
+
+
+Fraction& operator*=(const Fraction& f)
+
+Precondition: Requires a valid Fraction object and parameter that's a valid Fraction object or integer.
+Postcondition: Multiplies the Fraction object by the given parameter and returns the result.
+
+
+Fraction& operator/=(const Fraction& f)
+
+Precondition: Requires a valid Fraction object and parameter that's a valid Fraction object or integer.
+Precondition: Divides the Fraction object by the given parameter and returns the result.
+
+
+Fraction& operator++()
+
+usage: f++
+Precondition: Requires a valid Fraction object.
+Postcondition: Increments the Fraction object by 1 and returns the result.
+
+
+Fraction operator++(int)
+
+usage: ++f
+Precondition: Requires a valid Fraction object.
+Postcondition: Increments the Fraction object by 1 and returns the original value.
+
+
+Fraction& operator--()
+
+usage: f--
+Precondition: Requires a valid Fraction object.
+Postcondition: Decrements the Fraction object by 1 and returns the result.
+
+
+Fraction operator--(int)
+
+usage: --f 
+Precondition: Requires a valid Fraction object.
+Postcondition: Decrements the Fraction object by 1 and returns the original value.
+
+
+std::ostream& operator<<(std::ostream& out, const Fraction& f)
+
+Precondition: Requires a valid Fraction object.
+Postcondition: Prints the Fraction as either a whole number (5), a fraction (1/2), or a mixed number (1+1/2).
+
+
+std::istream &operator>>(std::istream &in, Fraction &f)
+
+Precondition: Requires a stream of fractions in valid format. Valid formats can be whole numbers (5), fractions (1/2), or mixed numbers (1+1/2).
+Postcondition: Returns a Fraction object parsed from the input.
+
+
+void testSimplify(int expectedNumerator, int expectedDenominator)
+
+Precondition: Requires a valid fraction and parameters specifying the expected numerator and denominator after simplification.
+Postcondition: Throws an assertion error if the simplified fraction does not match the expected values.
+
+*/
+
+
+
 #ifndef FRACTION_H
 #define FRACTION_H
 
@@ -31,13 +184,13 @@ namespace cs_fraction {
             Fraction operator--(int);
             
             friend std::ostream& operator<<(std::ostream& out, const Fraction& f);
-            friend std::istream& operator>>(std::istream& in, Fraction& f);
-
+            friend std::istream &operator>>(std::istream &in, Fraction &f);
 
             void testSimplify(int expectedNumerator, int expectedDenominator);
         private:
             void simplify();
             void normalize();
+            void parseMixedNumber(std::string input);
             int numerator;
             int denominator;
 
